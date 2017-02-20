2e79e1eb1ba79a1a241f74000395378478870a3c
dgrabNDR <dan.grabowski@nationaldebtrelief.com>
2017-02-20 11:44:18 -0500

    Update EncryptAndUpload.java

--- src/main/java/com/encryptandupload/EncryptAndUpload.java
+++ src/main/java/com/encryptandupload/EncryptAndUpload.java
@@ -12,7 +12,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.codec.binary.Base64;
-import org.bouncycastle.openpgp.PGPException;
+import org.bouncycastle.bcprov-jdk15on.openpgp.PGPException;
 
 //import com.didisoft.pgp.*;
 
