154469cefe960b7e2f050176e84c76d564bf7f25
dgrabNDR <dan.grabowski@nationaldebtrelief.com>
2017-03-24 14:02:09 -0400

    Add files via upload

--- src/main/java/com/encryptandupload/UploadFile.java
+++ src/main/java/com/encryptandupload/UploadFile.java
@@ -1,4 +1,4 @@
-package main.java.com.encryptandupload;
+package main.java.com.pulldocuments;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
@@ -15,6 +15,8 @@ import java.util.HashMap;
 import java.util.Map;
 
 import org.apache.commons.codec.binary.Base64;
+import org.apache.commons.net.ftp.FTPClient;
+import org.apache.commons.net.ftp.FTPReply;
 import org.apache.http.HttpHost;
 import org.apache.http.auth.AuthScope;
 import org.apache.http.auth.UsernamePasswordCredentials;
@@ -45,8 +47,7 @@ public class UploadFile {
 		
 		try{
 			// sets static ip addresses 
-			InetAddress ip = InetAddress.getLocalHost();
-			System.out.println("Normal IP Address => "+ip.getHostAddress());
+			System.out.println("Normal IP Address => "+InetAddress.getLocalHost().getHostAddress());
 			System.out.println("setting up fixie...");
 			URL fixie = new URL("http://"+System.getenv("FIXIE_SOCKS_HOST"));
 			String fixieInfo = fixie.getUserInfo();
@@ -58,32 +59,64 @@ public class UploadFile {
 			System.setProperty("socksProxyHost", fixie.getHost());
 			Authenticator.setDefault(new ProxyAuthenticator(fixieUser, fixiePassword));
 			
-			//ip = InetAddress.getLocalHost();
-			//System.out.println("Fixie IP Address => "+ip.getHostAddress());
-			
+			/*
 			System.out.println("setting up GenericFTPClient...");
+			
 			GenericFTPClient sftp = new GenericFTPClient();
 			System.out.println(sftp);
 			sftp.connect(params.get("ftphost"), params.get("ftpuser"), params.get("ftppass"), 22);
 			System.out.println("Change folder status:"+sftp.changeDir(params.get("ftpfolder")));
 
 			File att = lstAtt.get(0);
-			byte[] fileBytes = readFile(att);		      
-			InputStream is1 = new ByteArrayInputStream(fileBytes);		
+			byte[] fileBytes = readFile(att);
+		      
+			InputStream is1 = new ByteArrayInputStream(fileBytes);
 			
 			res = sftp.uploadFile(is1, (String) att.getName());
+
 			if(res){
 				System.out.println("upload successful");
 			}
 			System.out.println(res);
 
 			sftp.logout();
+			*/
+			FTPClient ftpClient = new FTPClient();
+	        try {
+	            ftpClient.connect(params.get("ftphost"));
+	            showServerReply(ftpClient);
+	            int replyCode = ftpClient.getReplyCode();
+	            if (!FTPReply.isPositiveCompletion(replyCode)) {
+	                System.out.println("Operation failed. Server reply code: " + replyCode);
+	                return;
+	            }
+	            boolean success = ftpClient.login(params.get("ftpuser"), params.get("ftppass"));
+	            showServerReply(ftpClient);
+	            if (!success) {
+	                System.out.println("Could not login to the server");
+	                return;
+	            } else {
+	                System.out.println("LOGGED IN SERVER");
+	            }
+	        } catch (IOException ex) {
+	            System.out.println("Oops! Something wrong happened");
+	            ex.printStackTrace();
+	        }
 		}
 		catch(Exception ex){
 			ex.printStackTrace();
 		}
 	}
 	
+	private static void showServerReply(FTPClient ftpClient) {
+        String[] replies = ftpClient.getReplyStrings();
+        if (replies != null && replies.length > 0) {
+            for (String aReply : replies) {
+                System.out.println("SERVER: " + aReply);
+            }
+        }
+    }
+
 	private class ProxyAuthenticator extends Authenticator {
 	  private final PasswordAuthentication passwordAuthentication;
 	  private ProxyAuthenticator(String user, String password) {
