7583ca9add96f577db7f412a3a817d2dc8b41fe3
dgrabNDR <dan.grabowski@nationaldebtrelief.com>
2017-03-24 15:42:05 -0400

    Update EncryptAndUpload.java

--- src/main/java/com/encryptandupload/EncryptAndUpload.java
+++ src/main/java/com/encryptandupload/EncryptAndUpload.java
@@ -75,10 +75,10 @@ public class EncryptAndUpload extends HttpServlet{
 		        	FileOutputStream fos = new FileOutputStream(newFile);
 		        }
 				if(suchEncrypt.renameTo(newFile)){
-					System.out.println("suchEncrypt: "+suchEncrypt.getName());
-					System.out.println("newFile: "+newFile.getName());
+					//System.out.println("suchEncrypt: "+suchEncrypt.getName());
+					//System.out.println("newFile: "+newFile.getName());
 					encryptedFiles.add(newFile);
-					encryptedFiles.add(suchEncrypt);
+					//encryptedFiles.add(suchEncrypt);
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
